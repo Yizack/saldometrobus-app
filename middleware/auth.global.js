c0000ee0b1b09f0428a351032b896075e62fe83e
Yizack Rangel <yizackr@gmail.com>
2023-04-28 11:52:45 +0200

    Use navigateTo helper

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,8 +2,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (!Auth().exists) {
     await Auth().restore();
     if (Auth().exists && to.path === "/") {
-      const { $router } = useNuxtApp();
-      $router.replace("/app/");
+      return navigateTo("/app/", { replace: true });
     }
   }
 });
