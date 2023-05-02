93d9347fb7e5986c22e41f6e0629e216ba4e8d1a
Yizack Rangel <yizackr@gmail.com>
2023-05-02 18:51:09 +0200

    Print routes length

--- miBusRutas.js
+++ miBusRutas.js
@@ -4,6 +4,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const result = await fetch("https://www.mibus.com.pa/wp-content/uploads/web-maps/routes.js");
 const response = await result.text();
 const routes = JSON.parse(response.replace("const datamap = ", "").replace(";", ""));
+console.info("Rutas MiBus: ", routes.length);
 writeFileSync("./assets/rutas.json", JSON.stringify(routes));
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 1;
