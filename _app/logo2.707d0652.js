ebc1fb8b7636c196145ef733bdbea2e6df419503
Yizack <yizackr@gmail.com>
2023-05-04 21:43:38 +0000

    Deploying to gh-pages from @ Yizack/saldometrobus-app@b323c9fe0887132764edbed010d52dc30b4e29b0 🚀

---
+++ _app/logo2.707d0652.js
@@ -0,0 +1,1 @@
+import"./entry.08beb14d.js";const o=""+globalThis.__publicAssetsURL("images/logo2.webp");export{o as _};
--- _app/logo2.88af8627.js
+++
@@ -1,1 +0,0 @@
-import"./entry.bfbb5dc8.js";const o=""+globalThis.__publicAssetsURL("images/logo2.webp");export{o as _};
