4dd228f966bc6038f253a4ab05080b7a9e4f657a
Yizack Rangel <yizackr@gmail.com>
2023-05-09 11:25:54 +0200

    chore: no scalable app

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       },
       meta: [
         { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" },
         { name: "robots", content: "noindex, nofollow" }
       ],
       link: [
