a3475190ad0f9295e570d4780b776bd5cf110713
Yizack Rangel <yizackr@gmail.com>
2024-11-07 17:43:12 -0500

    refactor: perfil to composition api

--- app/pages/app/perfil.vue
+++ app/pages/app/perfil.vue
@@ -1,5 +1,127 @@
 <script setup lang="ts">
 definePageMeta({ layout: "main" });
+const auth = Auth();
+
+const dialog = ref("");
+const edit = ref({
+  nombre: false
+});
+
+const user = ref({
+  nombre: auth.user.nombre,
+  email: auth.user.email,
+  token: auth.user.token,
+  tarjetas: [] as SaldometrobusTarjeta[]
+});
+
+const form = useFormState({
+  current_password: "",
+  new_password: "",
+  password_check: "",
+  error: false
+});
+
+const isPasswordValid = computed(() => {
+  return form.value.new_password.length >= 3 && form.value.current_password !== form.value.new_password && form.value.current_password.length > 0;
+});
+
+const isPasswordCheckValid = computed(() => {
+  return isPasswordValid.value && form.value.new_password === form.value.password_check;
+});
+
+const nombre = ref<HTMLInputElement>();
+const editName = async () => {
+  edit.value.nombre = !edit.value.nombre;
+  user.value.nombre = user.value.nombre.trim();
+  if (edit.value.nombre) {
+    const end = user.value.nombre.length;
+    nombre.value?.setSelectionRange(end, end);
+    nombre.value?.focus();
+    nombre.value?.removeAttribute("readonly");
+  }
+  else {
+    if (user.value.nombre !== auth.user.nombre) {
+      const { error, error_key } = await API.updateName({
+        nombre: user.value.nombre,
+        email: auth.user.email,
+        token: auth.user.token || ""
+      });
+
+      if (!error) {
+        auth.updateName(user.value.nombre);
+        await CAPACITOR.showToast(t("name_updated"));
+      }
+      else {
+        user.value.nombre = auth.user.nombre;
+        await CAPACITOR.showToast(t(error_key));
+      }
+    }
+    nombre.value?.setAttribute("readonly", "true");
+    nombre.value?.blur();
+  }
+};
+
+const current = ref<HTMLInputElement>();
+const updatePass = async () => {
+  if (isPasswordValid.value && isPasswordCheckValid.value) {
+    dialog.value = t("updating_pass");
+    showModal("progress-dialog");
+    const { error, error_key } = await API.userPassUpdate({
+      current_password: form.value.current_password,
+      new_password: form.value.new_password,
+      email: auth.user.email,
+      token: auth.user.token
+    });
+    if (!error) {
+      await CAPACITOR.showToast(t("pass_updated"), "long");
+      form.reset();
+    }
+    else {
+      if (error_key === "pass_error") {
+        form.value.error = true;
+        current.value?.focus();
+      }
+      await CAPACITOR.showToast(t(error_key), "long");
+    }
+  }
+  await sleep(0.5);
+  hideModal("progress-dialog");
+};
+
+const copyToken = (event: Event) => {
+  const input = event.currentTarget as HTMLInputElement;
+  input.select();
+  CAPACITOR.writeToClipboard(user.value.token);
+};
+
+const deleteAccount = async () => {
+  const confirm = await CAPACITOR.confirm(t("delete_account"), t("delete_account_sure"));
+  if (confirm) {
+    dialog.value = t("deleting_account");
+    showModal("progress-dialog");
+    const { error, error_key } = await API.deleteAccount({
+      email: auth.user.email,
+      token: auth.user.token
+    });
+    if (!error) {
+      await DB.deleteAll();
+      await auth.logout();
+      await CAPACITOR.showToast(t("account_deleted"));
+      await sleep(0.5);
+      hideModal("progress-dialog");
+      navigateTo("/", { replace: true });
+    }
+    else {
+      await CAPACITOR.showToast(t(error_key));
+      await sleep(0.5);
+      hideModal("progress-dialog");
+    }
+  }
+};
+
+onMounted(async () => {
+  user.value.tarjetas = await DB.getTarjetas();
+});
 </script>
 
 <template>
@@ -8,7 +130,7 @@ definePageMeta({ layout: "main" });
       <h4 class="text-primary-emphasis m-0"><b>{{ t("nombre") }}</b></h4>
       <div class="input-group p-2">
         <input ref="nombre" v-model="user.nombre" class="form-control py-2" type="text" readonly>
-        <button v-if="!Auth().isGuest" class="btn btn-sm" :class="edit.nombre ? 'btn-success' : 'btn-primary'" :style="{ width: '3rem' }" @click="editName()">
+        <button v-if="!auth.isGuest" class="btn btn-sm" :class="edit.nombre ? 'btn-success' : 'btn-primary'" :style="{ width: '3rem' }" @click="editName()">
           <Transition name="tab" mode="out-in">
             <Icon v-if="edit.nombre" name="check" />
             <Icon v-else name="edit" />
@@ -34,10 +156,10 @@ definePageMeta({ layout: "main" });
         <p v-else class="m-0">{{ t("no_tarjetas") }}</p>
       </div>
     </div>
-    <div v-if="!Auth().isGuest" class="bg-body-tertiary border rounded p-2 mb-2 shadow">
+    <div v-if="!auth.isGuest" class="bg-body-tertiary border rounded p-2 mb-2 shadow">
       <h4 class="text-primary-emphasis m-0"><b>{{ t("password") }}</b></h4>
       <div class="m-2">
-        <form novalidate @submit.prevent="updatePass()">
+        <form novalidate @submit.prevent="updatePass">
           <div class="mb-3 position-relative form-floating">
             <input ref="current" v-model="form.current_password" class="form-control" :class="{ 'is-invalid': form.error }" type="password" autocomplete="password" :placeholder="t('current_pass')" required @keyup="form.error = false">
             <label>{{ t("current_pass") }}</label>
@@ -59,147 +181,18 @@ definePageMeta({ layout: "main" });
         </form>
       </div>
     </div>
-    <div v-if="!Auth().isGuest" class="bg-body-tertiary border rounded p-2 shadow">
+    <div v-if="!auth.isGuest" class="bg-body-tertiary border rounded p-2 shadow">
       <h4 class="text-primary-emphasis m-0"><b>{{ t("account_id") }}</b></h4>
       <div class="m-2">
         <input :value="user.token" class="form-control py-2" type="text" readonly @click="copyToken($event)">
       </div>
     </div>
-    <div v-if="!Auth().isGuest" class="d-grid">
-      <button class="btn btn-danger mt-2" @click="deleteAccount()">{{ t("delete_account") }}</button>
+    <div v-if="!auth.isGuest" class="d-grid">
+      <button class="btn btn-danger mt-2" @click="deleteAccount">{{ t("delete_account") }}</button>
     </div>
-    <div v-if="Auth().isGuest" class="text-center mt-3">
+    <div v-if="auth.isGuest" class="text-center mt-3">
       <p class="small m-0"><small>{{ t("nota") }}</small></p>
     </div>
     <ProgressDialog :message="dialog" />
   </section>
 </template>
-
-<script lang="ts">
-export default {
-  data () {
-    return {
-      dialog: "",
-      edit: {
-        nombre: false
-      },
-      user: {
-        nombre: Auth().user.nombre,
-        email: Auth().user.email,
-        token: Auth().user.token,
-        tarjetas: [] as SaldometrobusTarjeta[]
-      },
-      form: {
-        current_password: "",
-        new_password: "",
-        password_check: "",
-        error: false
-      }
-    };
-  },
-  computed: {
-    isPasswordValid () {
-      return this.form.new_password.length >= 3 && this.form.current_password !== this.form.new_password && this.form.current_password.length > 0;
-    },
-    isPasswordCheckValid () {
-      return this.isPasswordValid && this.form.new_password === this.form.password_check;
-    }
-  },
-  async mounted () {
-    this.user.tarjetas = await DB.getTarjetas();
-  },
-  methods: {
-    async editName () {
-      this.edit.nombre = !this.edit.nombre;
-      this.user.nombre = this.user.nombre.trim();
-      const input = this.$refs.nombre as HTMLInputElement;
-      if (this.edit.nombre) {
-        const end = this.user.nombre.length;
-        input.setSelectionRange(end, end);
-        input.focus();
-        input.removeAttribute("readonly");
-      }
-      else {
-        if (this.user.nombre !== Auth().user.nombre) {
-          const { error, error_key } = await API.updateName({
-            nombre: this.user.nombre,
-            email: Auth().user.email,
-            token: Auth().user.token || ""
-          });
-
-          if (!error) {
-            Auth().updateName(this.user.nombre);
-            await CAPACITOR.showToast(t("name_updated"));
-          }
-          else {
-            this.user.nombre = Auth().user.nombre;
-            await CAPACITOR.showToast(t(error_key));
-          }
-        }
-        input.setAttribute("readonly", "true");
-        input.blur();
-      }
-    },
-    async updatePass () {
-      if (this.isPasswordValid && this.isPasswordCheckValid) {
-        this.dialog = t("updating_pass");
-        showModal("progress-dialog");
-        const { error, error_key } = await API.userPassUpdate({
-          current_password: this.form.current_password,
-          new_password: this.form.new_password,
-          email: Auth().user.email,
-          token: Auth().user.token
-        });
-        if (!error) {
-          await CAPACITOR.showToast(t("pass_updated"), "long");
-          this.form = {
-            current_password: "",
-            new_password: "",
-            password_check: "",
-            error: false
-          };
-        }
-        else {
-          if (error_key === "pass_error") {
-            this.form.error = true;
-            const current_input = this.$refs.current as HTMLInputElement;
-            current_input.focus();
-          }
-          await CAPACITOR.showToast(t(error_key), "long");
-        }
-      }
-      await sleep(0.5);
-      hideModal("progress-dialog");
-    },
-    copyToken (event: Event) {
-      const input = event.target as HTMLInputElement;
-      input.select();
-      CAPACITOR.writeToClipboard(this.user.token);
-    },
-    async deleteAccount () {
-      const confirm = await CAPACITOR.confirm(t("delete_account"), t("delete_account_sure"));
-      if (confirm) {
-        this.dialog = t("deleting_account");
-        showModal("progress-dialog");
-        const { error, error_key } = await API.deleteAccount({
-          email: Auth().user.email,
-          token: Auth().user.token
-        });
-        if (!error) {
-          await DB.deleteAll();
-          await Auth().logout();
-          await CAPACITOR.showToast(t("account_deleted"));
-          await sleep(0.5);
-          hideModal("progress-dialog");
-          this.$router.replace("/");
-        }
-        else {
-          await CAPACITOR.showToast(t(error_key));
-          await sleep(0.5);
-          hideModal("progress-dialog");
-        }
-      }
-    }
-  }
-};
-</script>
