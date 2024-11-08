29bb2dbba80343eafcbd2fad7cd6438f8ef11978
Yizack Rangel <yizackr@gmail.com>
2024-11-08 06:44:57 -0500

    chore: capacitor config types

---
+++ capacitor.config.ts
@@ -0,0 +1,44 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+export default {
+  appId: "com.yizack.saldometrobus",
+  appName: "Saldo Metrobús Panamá",
+  webDir: ".output/public",
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: "https",
+    hostname: "saldometrobus.yizack.com"
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    },
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 200,
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: false,
+      splashImmersive: false,
+      backgroundColor: "#F8F9FC"
+    },
+    CapacitorSQLite: {
+      iosDatabaseLocation: "Library/CapacitorDatabase",
+      iosIsEncryption: false,
+      iosKeychainPrefix: "cap",
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: false,
+      androidBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite",
+        biometricSubTitle: "Log in using your biometric"
+      },
+      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+      electronMacLocation: "Yizack/CapacitorDatabases",
+      electronLinuxLocation: "Databases"
+    }
+  }
+} satisfies CapacitorConfig;
